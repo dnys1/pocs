b25337d93b6e7708e5414faed9cf2149d7b049ad
Dillon Nys <dillon.andre.nys@gmail.com>
2021-09-14 07:41:47 -0700

    flutter_map test

--- amplify_geo/lib/src/amplify_geo.dart
+++ amplify_geo/lib/src/amplify_geo.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:amplify_common/amplify_common.dart';
 import 'package:aws_signature_v4/aws_signature_v4.dart';
-import 'package:maplibre_gl/maplibre_gl.dart';
+import 'package:http/http.dart' as http;
+import 'package:vector_map_tiles/vector_map_tiles.dart';
+import 'package:vector_tile_renderer/vector_tile_renderer.dart';
 
 class AmplifyGeo {
   static final AmplifyGeo instance = AmplifyGeo._();
@@ -12,52 +17,71 @@ class AmplifyGeo {
   void configure(AmazonLocationServicesPluginConfig config) {
     _config = config;
   }
+}
 
-  TransformRequestFunction getTransformRequestFunction(
-    AWSCredentials credentials,
-  ) {
-    var config = _config;
+class AmplifyGeoProvider extends VectorTileProvider {
+  AmplifyGeoProvider(AWSCredentials credentials)
+      : _signer = AWSSigV4Signer(credentials),
+        _client = http.Client();
+
+  final AWSSigV4Signer _signer;
+  final http.Client _client;
+
+  @override
+  int get maximumZoom => 14;
+
+  @override
+  Future<Uint8List> provide(TileIdentity tile) async {
+    var config = AmplifyGeo.instance._config;
     if (config == null) {
-      throw StateError('Must call configure first');
+      throw StateError('Must call AmplifyGeo.instance.configure first');
     }
-    final signer = AWSSigV4Signer(credentials);
+    final host = 'maps.geo.${config.region}.amazonaws.com';
+    final signerRequest = AWSSignerRequest(
+      AWSHttpRequest(
+        method: HttpMethod.get,
+        host: host,
+        path: '/maps/v0/maps/${config.maps!.$default}/tiles/${tile.z}/${tile.x}/${tile.y}',
+        headers: {
+          AWSHeaders.host: host,
+        },
+      ),
+      credentialScope: AWSCredentialScope(
+        region: config.region!,
+        service: 'geo',
+      ),
+      presignedUrl: true,
+    );
+    final signedRequest = _signer.sign(signerRequest);
+    final response = await signedRequest.send(_client);
+    return response.bodyBytes;
+  }
 
-    return (
-      String url,
-      ResourceType resourceType,
-    ) {
-      if (resourceType == ResourceType.style && !url.contains('://')) {
-        url =
-            'https://maps.geo.${config.region}.amazonaws.com/maps/v0/maps/$url/style-descriptor';
-      }
-
-      if (url.contains('amazonaws.com')) {
-        final uri = Uri.parse(url);
-        final signerRequest = AWSSignerRequest(
-          AWSHttpRequest(
-            method: HttpMethod.get,
-            host: uri.host,
-            path: uri.path,
-            queryParameters: uri.queryParameters,
-            headers: {
-              AWSHeaders.host: uri.host,
-            },
-          ),
-          credentialScope: AWSCredentialScope(
-            region: config.region!,
-            service: 'geo',
-          ),
-          presignedUrl: true,
-        );
-        final signedRequest = signer.sign(signerRequest);
-        return {
-          'url': signedRequest.toString(),
-          'credentials': 'same-origin',
-          'headers': <String, dynamic>{},
-          'method': 'GET',
-          'collectResourceTiming': false,
-        };
-      }
-    };
+  Future<Theme> getTheme({Logger? logger}) async {
+    var config = AmplifyGeo.instance._config;
+    if (config == null) {
+      throw StateError('Must call AmplifyGeo.instance.configure first');
+    }
+    final host = 'maps.geo.${config.region}.amazonaws.com';
+    final signerRequest = AWSSignerRequest(
+      AWSHttpRequest(
+        method: HttpMethod.get,
+        host: host,
+        path: '/maps/v0/maps/${config.maps!.$default}/style-descriptor',
+        headers: {
+          AWSHeaders.host: host,
+        },
+      ),
+      credentialScope: AWSCredentialScope(
+        region: config.region!,
+        service: 'geo',
+      ),
+      presignedUrl: true,
+    );
+    final signedRequest = _signer.sign(signerRequest);
+    final response = await signedRequest.send(_client);
+    final json = jsonDecode(response.body) as Map<String, dynamic>;
+    final theme = ThemeReader(logger: logger).read(json);
+    return theme;
   }
 }
--- amplify_geo/lib/src/amplify_geo_map.dart
+++ amplify_geo/lib/src/amplify_geo_map.dart
@@ -4,9 +4,13 @@ import 'package:amplify_common/amplify_common.dart';
 import 'package:amplify_geo/src/amplify_geo.dart';
 import 'package:aws_cognito/aws_cognito.dart';
 import 'package:aws_signature_v4/aws_signature_v4.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Theme;
 import 'package:flutter/widgets.dart';
-import 'package:maplibre_gl/maplibre_gl.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:vector_map_tiles/vector_map_tiles.dart';
+import 'package:vector_tile_renderer/vector_tile_renderer.dart';
+import 'package:vector_tile_renderer/src/logger.dart';
 
 class AmplifyGeoMap extends StatefulWidget {
   const AmplifyGeoMap({
@@ -26,8 +30,8 @@ class AmplifyGeoMap extends StatefulWidget {
   }) : super(key: key);
 
   final AmplifyConfig config;
-  final Coordinates? center;
-  final int? zoom;
+  final LatLng? center;
+  final double? zoom;
   final List<String>? customAttribution;
   final bool? dragPan;
   final bool? dragRotate;
@@ -88,23 +92,68 @@ class _AmplifyGeoMapState extends State<AmplifyGeoMap> {
           return const Center(child: CircularProgressIndicator());
         }
         final credentials = snapshot.data!;
-        return MaplibreMap(
-          center: widget.center,
-          zoom: widget.zoom,
-          style: _config.maps!.$default,
-          transformRequestFunction:
-              AmplifyGeo.instance.getTransformRequestFunction(credentials),
-          customAttribution: widget.customAttribution,
-          dragPan: widget.dragPan,
-          dragRotate: widget.dragRotate,
-          doubleClickZoom: widget.doubleClickZoom,
-          hash: widget.hash,
-          fadeDuration: widget.fadeDuration,
-          failIfMajorPerformanceCaveat: widget.failIfMajorPerformanceCaveat,
-          interactive: widget.interactive,
-          keyboard: widget.keyboard,
-        );
+        var flags = InteractiveFlag.none;
+        if (widget.interactive ?? false) {
+          if (widget.dragPan ?? true) {
+            flags |= InteractiveFlag.drag;
+          }
+          if (widget.doubleClickZoom ?? true) {
+            flags |= InteractiveFlag.doubleTapZoom;
+          }
+          if (widget.dragRotate ?? true) {
+            flags |= InteractiveFlag.rotate;
+          }
+        }
+        final provider = AmplifyGeoProvider(credentials);
+        const logger = ConsoleLogger();
+        return FutureBuilder<Theme>(
+            future: provider.getTheme(logger: logger),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(child: Text(snapshot.error!.toString()));
+              } else if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              return FlutterMap(
+                options: MapOptions(
+                  center: widget.center,
+                  zoom: widget.zoom ?? 13,
+                  interactiveFlags: flags,
+                  plugins: [
+                    VectorMapTilesPlugin(),
+                  ],
+                ),
+                layers: [
+                  VectorTileLayerOptions(
+                    tileProvider: provider,
+                    theme: snapshot.data!,
+                    logCacheStats: true,
+                    // showTileDebugInfo: true,
+                  ),
+
+                  // if (widget.center != null)
+                  // MarkerLayerOptions(markers: [
+                  //   Marker(point: widget.center!, builder: ),
+                  // ]),
+                ],
+              );
+            });
       },
     );
+  }
+}
+
+class ConsoleLogger implements Logger {
+  const ConsoleLogger();
+
+  @override
+  void log(MessageFunction message) {
+    print(message());
+  }
+
+  @override
+  void warn(MessageFunction message) {
+    final t = message();
+    print('WARN: $t');
   }
 }
