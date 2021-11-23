5520eb54c104508a2dc09bbf1f0e806af41a8d02
Dillon Nys <nydillon@amazon.com>
2021-11-23 14:51:50 -0700

    Update zone description

--- aws_signature_v4/lib/src/signer/zone.dart
+++ aws_signature_v4/lib/src/signer/zone.dart
@@ -1,7 +1,7 @@
 part of 'aws_signer.dart';
 
-/// Runs the given sign function in a zone where [print] defers to [safePrint]
-/// and uncaught exceptions are scrubbed of sensitive data.
+/// Runs the given sign function in a [Zone] where [print] defers to [safePrint]
+/// in order to prevent accidental exposure of secrets.
 R _signZoned<R>(R Function() signFn) {
   return runZoned(signFn, zoneSpecification: ZoneSpecification(
     print: (Zone self, ZoneDelegate parent, Zone zone, String line) {
